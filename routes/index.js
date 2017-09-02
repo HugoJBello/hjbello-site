b47ad379611d385caa443986dbbcddcbf3fbec0b
HugoJBello <hugo.bello.gu@gmail.com>
2017-09-02 19:01:04 +0200

    adding web content

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'index' });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'projects' });
 });
 
 module.exports = router;
